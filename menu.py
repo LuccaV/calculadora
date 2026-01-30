41d500a714a96457acffe903e288f277dce8fe32
LuccaV <lleiteviegas@gmail.com>
2026-01-30 14:40:05 -0300

    commit3

--- menu.py
+++ menu.py
@@ -6,6 +6,23 @@ def mostrar_menu():
     print("4 - divisão")
     print("0 - sair")
 
+def adicao(a, b):
+    return a + b
+
+
+def subtracao(a, b):
+    return a - b
+
+
+def multiplicacao(a, b):
+    return a * b
+
+
+def divisao(a, b):
+    if b == 0:
+        return "erro"
+    return a / b
+
 
 def main():
     while True:
@@ -15,6 +32,19 @@ def main():
         if opcao == "0":
             print("saindo...")
             break
+
+        a = float(input("Digite o primeiro número: "))
+        b = float(input("Digite o segundo número: "))
+
+        if opcao == "1":
+            print("Resultado:", adicao(a, b))
+        elif opcao == "2":
+            print("Resultado:", subtracao(a, b))
+        elif opcao == "3":
+            print("Resultado:", multiplicacao(a, b))
+        elif opcao == "4":
+            print("Resultado:", divisao(a, b))
+            
         else:
             print("opção inválida!")
 
